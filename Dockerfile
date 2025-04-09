37ddde11dea5ec4046c00e781612b10a990a2a92
Marwa Hafsi <marwa1haf@gmail.com>
2025-04-09 18:33:17 +0200

    restructuring _ going au bout!

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# For permission issues with docker : newgrp docker
 # Base image for Python 3.12
 FROM python:3.12-slim
 
@@ -5,13 +6,15 @@ FROM python:3.12-slim
 WORKDIR /app
 COPY . /app
 
-RUN pip install --upgrade pip
-
-# Install dependencies
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --upgrade pip && \
+pip install --no-cache-dir -r requirements.txt
 
 # Exposer le port que FastAPI utilisera
-EXPOSE 8000
+# EXPOSE 8000
 
 # Lancer l'application FastAPI avec Uvicorn
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+# CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+
+
+# Command to run (teste à la fois l'entraînement et la prédiction)
+CMD ["sh", "-c", "python  src/predict.py"]
--- src/train.py
+++ src/train.py
@@ -9,17 +9,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import resample
 from xgboost import XGBClassifier
+import os
+import pickle
 
-# Charger le dataset
-df = pd.read_csv(
-    "https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv"
-)
 
-# Afficher les premières lignes
-df.head()
+# ---  Charger le dataset ---
+DATA_PATH = os.path.join("data", "creditcard.csv")
+if not os.path.exists(DATA_PATH):
+    df = pd.read_csv(
+        "https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv"
+    )
+    df.to_csv(DATA_PATH, index=False)
+    print("Dataset téléchargé dans 'data/'")
+else:
+    df = pd.read_csv(DATA_PATH)
+    print("Dataset présent dans 'data/'")
 
-# Statistiques descriptives
-df.describe()
+# --- Analyse exploratoire ---
+df.head()  # Afficher les premières lignes
+df.describe()  # Statistiques descriptives
 
 # Nombre de fraudes vs transactions normales
 fraud_count = df["Class"].value_counts()
@@ -30,9 +38,11 @@ plt.xticks([0, 1], ["Normal (0)", "Fraude (1)"])
 plt.show()
 
 # Correlation des variables
-# plt.figure(figsize=(12, 8))
-# sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
-# plt.show()
+plt.figure(figsize=(12, 8))
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
+plt.show()
+
+# --- Pré-traitement ---
 
 # Vérifier s'il y a des valeurs manquantes
 missing_values = df.isnull().sum().sum()
@@ -45,14 +55,16 @@ print(f"Nombre total de valeurs aberrantes : {outliers.sum()}")
 # Standardiser la colonne 'Amount'
 scaler = StandardScaler()
 df["Amount"] = scaler.fit_transform(df[["Amount"]])
+df = df.drop(columns=["Time"])  # colonne 'Time' n'apporte pas de valeur utile
 
-# Supprimer la colonne 'Time' qui n'apporte pas de valeur utile
-df = df.drop(columns=["Time"])
+# Sauvegarde du scaler pour la prédiction
+with open(os.path.join("models", "scaler.pkl"), "wb") as f:
+    pickle.dump(scaler, f)
 
 
-# Under-sampling : Réduire le nombre de transactions normales.
-# Sur-échantillonnage (SMOTE) : Générer plus de fraudes artificielles.
+# --- Rééquilibrage des Classes ---
 def under_sampling(df):
+    """Under-sampling : Réduire le nombre de transactions normales."""
     # Séparer les classes
     df_fraud = df[df["Class"] == 1]  # Fraudes
     df_normal = df[df["Class"] == 0]  # Transactions normales
@@ -71,6 +83,7 @@ def under_sampling(df):
 
 
 def over_sampling(df):
+    """Sur-échantillonnage (SMOTE) : Générer plus de fraudes artificielles."""
     # Séparer les features et la target
     X = df.drop(columns=["Class"])
     y = df["Class"]
@@ -89,58 +102,66 @@ def over_sampling(df):
 df_balanced = under_sampling(df)
 df_resampled = over_sampling(df)
 
+#  --- Entrainement du model  ---
+
 # Séparer les features (X) et la cible (y)
 X = df_balanced.drop(columns=["Class"])  # Variables explicatives
 y = df_balanced["Class"]  # Variable cible (0 = normal, 1 = fraude)
-
 # Découpage en train/test
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42, stratify=y
 )
-
 print(f"Taille du train : {X_train.shape}, Taille du test : {X_test.shape}")
 
-# Entrainement du model
 # 3 Algorithmes :
 # Régression Logistique (baseline)
 # Random Forest(bon compromis entre simplicité et performance)
 # XGBoost(souvent performant sur ce genre de problème)
 
 # M1 : Entraînement du modèle
-log_reg = LogisticRegression()
-log_reg.fit(X_train, y_train)
+def Logisitic_Regression(X_train, X_test, y_train, y_test):
+    log_reg = LogisticRegression()
+    log_reg.fit(X_train, y_train)
 
-# Prédiction
-y_pred_log = log_reg.predict(X_test)
+    # Prédiction
+    y_pred_log = log_reg.predict(X_test)
 
-# Évaluation
-print("🔹 Régression Logistique 🔹")
-print(classification_report(y_test, y_pred_log))
+    # Évaluation
+    print("🔹 Régression Logistique 🔹")
+    print(classification_report(y_test, y_pred_log))
+    return log_reg
 
 # M2 : Entraînement du modèle
-rfc = RandomForestClassifier()
-rfc.fit(X_train, y_train)
+def Random_Forest(X_train, X_test, y_train, y_test):
+    rfc = RandomForestClassifier()
+    rfc.fit(X_train, y_train)
 
-# Prédiction
-y_pred_rfc = rfc.predict(X_test)
+    # Prédiction
+    y_pred_rfc = rfc.predict(X_test)
 
-# Évaluation
-print("🔹 Random Forest 🔹")
-print(classification_report(y_test, y_pred_rfc))
+    # Évaluation
+    print("🔹 Random Forest 🔹")
+    print(classification_report(y_test, y_pred_rfc))
+    return rfc
 
 # M3 : Entraînement du modèle
-xgb = XGBClassifier()
-xgb.fit(X_train, y_train)
+def XGBoost(X_train, X_test, y_train, y_test):
+    xgb = XGBClassifier()
+    xgb.fit(X_train, y_train)
 
-# Prédiction
-y_pred_xgb = xgb.predict(X_test)
+    # Prédiction
+    y_pred_xgb = xgb.predict(X_test)
 
-# Évaluation
-print("🔹 XGBoost 🔹")
-print(classification_report(y_test, y_pred_xgb))
+    # Évaluation
+    print("🔹 XGBoost 🔹")
+    print(classification_report(y_test, y_pred_xgb))
+    return xgb 
 
-import pickle
+# Established that random forest is better here.
+log_reg =Logisitic_Regression(X_train, X_test, y_train, y_test)
+rfc = Random_Forest(X_train, X_test, y_train, y_test)
+xgb = XGBoost(X_train, X_test, y_train, y_test)
 
 # Sauvegarde du modèle
-with open("model.pkl", "wb") as file:
+with open("models/model.pkl", "wb") as file:
     pickle.dump(rfc, file)  # Remplace "xgb" par ton meilleur modèle
